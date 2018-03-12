fffc79c7263e238935ad42b35f9ece43619b1078
Dan Sahagian <dan@dansahagian.com>
2018-03-12 16:38:22 -0700

    adds as license, readme, and comments

--- dydns.py
+++ dydns.py
@@ -1,17 +1,19 @@
 import os
 import requests
 
-# configuration
+# configuration, set DOMAIN and NAMECHEAP_DDNS_PW as env vars
 HOST = '@'
 DOMAIN = os.environ['DOMAIN']
 PW = os.environ['NAMECHEAP_DDNS_PW']
 ENDPOINT = 'https://dynamicdns.park-your-domain.com/update?'
 
 
+# get the IP address of the system
 def get_ip():
     return requests.get('https://dynamicdns.park-your-domain.com/getip').text
 
 
+# update Namecheap with your current IP address
 def main():
     payload = {'host': HOST, 'domain': DOMAIN, 'password': PW, 'ip': get_ip()}
     r = requests.get(ENDPOINT, params=payload)
